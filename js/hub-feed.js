721a3370301dfb9d94fa8fe257b4a11d6be43f3c
MCC\bmonreal <bmonreal@gremlinA>
2017-09-09 17:56:12 -0400

    The hub feed is pulling longitude and latitude and sorting events by distance

--- js/hub-feed.js
+++ js/hub-feed.js
@@ -1,5 +1,3 @@
-//$(document).ready(function() {
-
 var config = {
   apiKey: "AIzaSyAGrEKN1Msn1IQeAs_Ctw4SbM-yX40fGPc",
   authDomain: "findyourjam-2008f.firebaseapp.com",
@@ -12,50 +10,101 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-//pull current users location
-//pull events from firebase based on distance from current users location
+var keyArray = [];
+var distArray = [];
+var sorted;
 
- var query = database.ref("events/").orderByKey();
+console.log("Distance Array: " + distArray);
+console.log("Key Array: " + keyArray);
+
+var query = database.ref("events/").orderByKey();
   query.once("value").then(function(snapshot) {
-    console.log(snapshot);
+    //console.log(snapshot);
     snapshot.forEach(function(childSnapshot) {
 
       var key = childSnapshot.key;
-      var event = childSnapshot.val().title;
-      var location = childSnapshot.val().location;
-      var date = childSnapshot.val().date;
-      var description = childSnapshot.val().description;
-      var link = childSnapshot.val().link;
-      var user = childSnapshot.val().user;
-      var imageSrc = childSnapshot.val().image;
+      var eventLon = childSnapshot.val().eventLon;
+      var eventLat = childSnapshot.val().eventLat;
+      var userLon = localStorage.getItem("userLon");
+      var userLat = localStorage.getItem("userLat");
 
-      //console.log("key: " + key);
-      //console.log("event: " + event);
-      //console.log("location: " + location);
-      //console.log("date: " + date);
-      //console.log("description: " + description);
-      //console.log("link: " + link);
-      //console.log("user: " + user);
-
-      addEvent(key, imageSrc, event, date, description, location, link);
+      var eventDistance = distance(userLon, userLat ,eventLon, eventLat); 
+            
+      keyArray.push(key);
 
   });
 
 });
 
-//display events from firebase
-
+//display events from firebase db
 var addEvent = function(key,imageSrc, event, date, description, location, link) {
-      $("#event").append("<div class='event-medium-container' id='event-container" + key + "'><img class='event-medium-image' id='event-picture" + key + "' src='" + imageSrc + "'><h3 class='event-medium-title' id='event-title" + key + "'>" + event + "</h3><p class='event-medium-description' id='event-description" + key + "'>Description: " + description + "</p><p class='event-medium-date' id='event-date" + key + "'>Date: " + date + "</p><p class='event-medium-location' id='event-location" + key + "'>Location: " + location + "</p><p class='event-medium-link' id='event-link" + key + "'>Check Us Out: " + link +"</p></div>")
+  $("#event").append("<div class='row event-medium-container' id='event-container" + key + "'><div class='col-xs-12'><h3 class='event-medium-title' id='event-title" + key + "'>" + event + "</h3></div><div class='col-xs-2'><img class='event-medium-image' id='event-picture" + key + "' src='" + imageSrc + "'></div><div class='col-xs-8'><p class='event-medium-description' id='event-description" + key + "'>Description: " + description + "</p><p class='event-medium-date' id='event-date" + key + "'>Date: " + date + "</p><br><p class='event-medium-location' id='event-location" + key + "'>Location: " + location + "</p><br><a href='" + link + "' class='event-medium-link' id='event-link" + key + "'>Check Us Out</a></div></div>");
 
-      $("#event-container" + key).attr("key", key);
+  $("#event-container" + key).attr("key", key);
 
+};
+
+var loadSortedEvents = function() {
+  for (var i = 0; i < keyArray.length; i++) {
+    var query = database.ref("events/" + keyArray[i]);
+      query.once("value").then(function(snapshot) {
+
+        var event = snapshot.val().title;
+        var location = snapshot.val().location;
+        var date = snapshot.val().date;
+        var description = snapshot.val().description;
+        var link = snapshot.val().link;
+        var user = snapshot.val().user;
+        var imageSrc = snapshot.val().imageSRC;
+
+        addEvent(keyArray[i], imageSrc, event, date, description, location, link);
+
+      });
+  }
 };
 
 $(document.body).on("click", ".event-medium-container", function() {
     var event = $(this).attr("key");
+    localStorage.setItem("eventKey", event);
     //window.location.href to push the page or reload html?
     console.log("You clicked on event: " + event);
 });
 
-//});
+var distance = function(userLon, userLat, eventLon, eventLat) {
+  var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + userLon + "," + userLat + "&destinations=" + eventLon + "," + eventLat + "&key=AIzaSyAGrEKN1Msn1IQeAs_Ctw4SbM-yX40fGPc";
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      distArray.push(response.rows[0].elements[0].distance.value);
+  });
+};
+
+var bubbleSort = function(distArr, keyArr) {
+
+  var end = distArr.length - 1;
+  sorted = true;
+  for (var i = 0; i < end; i++) {
+    if (distArr[i] > distArr[i + 1]) {
+      var tempDist = distArr[i];
+      distArr[i] = distArr[i + 1];
+      distArr[i + 1] = tempDist;
+      var tempKey = keyArr[i];
+      keyArr[i] = keyArr[i + 1];
+      keyArr[i + 1] = tempKey;
+      sorted = false;
+      console.log("Distance Array: " + distArray);
+      console.log("Key Array: " + keyArray);
+    }
+  }
+};
+
+var bubbleCall = function() {
+  do {
+    bubbleSort(distArray, keyArray);
+  } while (!sorted);
+}
+
+setTimeout(bubbleCall, 1000 * 3);
+setTimeout(loadSortedEvents, 1000 * 5);
